a7f813263b8bdeee81d6524c8eb3d97fdc4d4b19
Dave <davidgrace00@gmail.com>
2022-01-28 16:09:25 +0000

    Added scoped service

--- RoundTheCode.HostedServiceExample/HostedServices/MyBackgroundService.cs
+++ RoundTheCode.HostedServiceExample/HostedServices/MyBackgroundService.cs
@@ -1,13 +1,31 @@
-﻿namespace RoundTheCode.HostedServiceExample.HostedServices
+﻿using RoundTheCode.HostedServiceExample.Services;
+
+namespace RoundTheCode.HostedServiceExample.HostedServices
 {
     public class MyBackgroundService : BackgroundService
     {
+        private readonly IMySingletonService _mySingletonService;
+        private readonly IMyTransientService _myTransientService;
+        private readonly IServiceProvider _serviceProvider;
+
+        public MyBackgroundService(IMySingletonService mySingletonService, IMyTransientService myTransientService, IServiceProvider serviceProvider)
+        {
+            _mySingletonService = mySingletonService;
+            _myTransientService = myTransientService;
+            _serviceProvider = serviceProvider;
+        }
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                Console.WriteLine(string.Format("{0} - {1}", "MyBackgroundService", DateTime.UtcNow.ToString("HH:mm:ss")));
-                await Task.Delay(new TimeSpan(0, 0, 1));
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var scopedService = scope.ServiceProvider.GetRequiredService<IMyScopedService>();
+
+                    Console.WriteLine(string.Format("{0} - {1}", "MyBackgroundService", DateTime.UtcNow.ToString("HH:mm:ss")));
+                    await Task.Delay(new TimeSpan(0, 0, 1));
+                }
             }
         }
     }
